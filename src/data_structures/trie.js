d15e7bb3830d69abe465400614b9a8048df6b336
Erica Norris <norrise120@gmail.com>
2020-06-23 16:54:03 -0700

    pass tests

--- src/data_structures/trie.js
+++ src/data_structures/trie.js
@@ -15,13 +15,61 @@ class Trie {
   }
 
   addWord(word) {
-    const code = this.buildCode(word);
+    const code = this.buildCode(word).split('');
+    let node = this._root;
+    code.forEach((char) => {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
+    });
+    if (node.words.indexOf(word) === -1) {
+      node.words.push(word);
+      this._count += 1;
+    }
   }
 
   lookupCode(code) {
+    let node = this._root;
+    const splitCode = code.split('');
+    for (let char of splitCode) {
+      if (!node.children[char]) {
+        return [];
+      }
+      node = node.children[char];
+    }
+    return node.words;
+  }
+
+  traverseSubtries(node, results) {
+    node.words.forEach((word) => {
+      results.push(word);
+    });
+    if (!Object.keys(node.children).length) {
+      return;
+    }
+    Object.keys(node.children).forEach((key) => {
+      this.traverseSubtries(node.children[key], results);
+    });
   }
 
   lookupPrefix(codePrefix) {
+    let node = this._root;
+
+    let results = [];
+    const splitCode = codePrefix.split('');
+    for (let char of splitCode) {
+      if (!node.children[char]) {
+        return results;
+      }
+      node = node.children[char];
+    }
+    if (Object.keys(node.children).length) {
+      this.traverseSubtries(node, results);
+    } else {
+      results = node.words;
+    }
+    return results;
   }
 
   count() {
